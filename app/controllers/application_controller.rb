610881cde4c83d34e90be926799589c74d93107e
mina-kato <kato1014sueko@gmail.com>
2020-10-28 02:41:05 +0000

    improvement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
   
-   helper_method :method_name
+   helper_method :logged_in?
+   helper_method :current_user
    
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(session_params)
-    if user && user.authenticate(session_params)
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -22,7 +22,8 @@ class SessionsController < ApplicationController
   def log_in(user)
     session[:user_id] = user.id
   end
-  def user_params
+  
+  def session_params
     params.require(:session).permit(:email, :password)
   end
   
