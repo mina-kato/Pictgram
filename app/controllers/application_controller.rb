d66aeb5726c84317144aa9eda464245d67a58d60
mina-kato <kato1014sueko@gmail.com>
2020-11-02 01:05:13 +0000

    rails_06

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
   
-   helper_method :logged_in?
-   helper_method :current_user
+   helper_method :logged_in?, :current_user
    
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,17 @@
 class User < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, 
-                     length: {maximum: 15 }
-    validates :email, presence: true
+  VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
 
-VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
-validates :password, presence: true, 
+  validates :name, presence: true, uniqueness: true, length: {maximum: 15 }
+  validates :email, presence: true
+  validates :password, presence: true, 
                      length: {minimum: 8, maximum: 32 },
                      format: {
                          with: VALID_PASSWORD_REGEX
                      },
                      allow_blank: true
-
-VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
-
-
-
-    has_secure_password
+                     
+  has_secure_password
+  
+  has_many :topics
 end
